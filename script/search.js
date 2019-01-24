acc33eec07298c56187c76a12ed50f322e868df6
rotarurobert <rotaru_robert97@yahoo.com>
2019-01-24 17:55:07 +0200

    dinamic search

--- script/search.js
+++ script/search.js
@@ -1,17 +1,18 @@
 function myFunction() {
-  var input, filter, grid, div, i;
+  var input, filter, table, div, p, i, txtValue;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
-  grid = document.getElementById("myGrid");
-  div = grid.getElementsByTagName("div");
+  table = document.getElementById("myGrid");
+  div = table.getElementsByTagName("div");
   for (i = 0; i < div.length; i++) {
-      p = div.getElementsByTagName("p")[0];
-      if(p){
-        if (p.innerHTML.toUpperCase().indexOf(filter) > -1) {
-          div[i].style.display = "";
-        } else {
-          div[i].style.display = "none";
-        }
+    p = div[i].getElementsByTagName("p")[0];
+    if (p) {
+      txtValue = p.textContent || p.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        div[i].style.display = "";
+      } else {
+        div[i].style.display = "none";
       }
-  }       
+    }       
+  }
 }
